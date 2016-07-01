f4f4cef545e7b538d7f297969f3eae5349d4725e
Alexey Polishchuk <yohapoha@gmail.com>
2016-07-01 11:28:04 +0300

    w

--- app/js/main/ts/main.js
+++ app/js/main/ts/main.js
@@ -13,10 +13,24 @@ function centralizeV(object, content) {
     var parentHeight = parent.height();
     var objectHeight = object.height();
     padding += Math.floor((parentHeight - objectHeight) / 2);
-    console.log(parentHeight);
     object.css("padding-top", padding);
+}
+function tableZebra() {
+    var table = $(".table_zebra");
+    table.map(function (index, element) {
+        var table = $(element);
+        var length = table.children().length;
+        var rows = table.children();
+        for (var i = 0; i < length; i += 2) {
+            rows.eq(i).hasClass("table-row_head") || rows.eq(i).addClass("table-row_highlight");
+        }
+    });
 }
 centralizeH($(".js_centralizeH"));
 centralizeV($(".js_centralizeV"));
 centralizeV($(".worktime"), true);
+centralizeV($(".warranty"), true);
+centralizeV($(".contacts"), true);
+
+tableZebra();
 //# sourceMappingURL=main.js.map
--- app/modules/main/ts/main.ts
+++ app/modules/main/ts/main.ts
@@ -1,21 +1,35 @@
 /// <reference path="../../../../typings/jquery/jquery.d.ts" />
 
 function centralizeH(object:JQuery) {
-    var parent:JQuery = object.parent();
-    var parentWidth:number = parent.width();
-    var objectWidth:number = object.outerWidth(true);
-    var margin = Math.floor((parentWidth - objectWidth)/2);
-    object.css("margin-left", margin);
+	var parent:JQuery = object.parent();
+	var parentWidth:number = parent.width();
+	var objectWidth:number = object.outerWidth(true);
+	var margin = Math.floor((parentWidth - objectWidth)/2);
+	object.css("margin-left", margin);
 }
 function centralizeV(object:JQuery, content:boolean = false) {
-    var padding = content?Math.floor($(".navigation").height()/2):0;
-    var parent:JQuery = content?object.parent().parent():object.parent();
-    var parentHeight:number = parent.height();
-    var objectHeight:number = object.height();
-    padding += Math.floor((parentHeight - objectHeight)/2);
-    console.log(parentHeight)
-    object.css("padding-top", padding);
+	var padding:number = content?Math.floor($(".navigation").height()/2):0;
+	var parent:JQuery = content?object.parent().parent():object.parent();
+	var parentHeight:number = parent.height();
+	var objectHeight:number = object.height();
+	padding += Math.floor((parentHeight - objectHeight)/2);
+	object.css("padding-top", padding);
+}
+function tableZebra() {
+	var table = $(".table_zebra");
+	table.map(function(index, element) {
+		var table = $(element);
+		var length = table.children().length;
+		var rows = table.children();
+		for(var i = 0; i < length; i+=2) {
+			rows.eq(i).hasClass("table-row_head") || rows.eq(i).addClass("table-row_highlight");
+		}
+	})
 }
 centralizeH($(".js_centralizeH"));
 centralizeV($(".js_centralizeV"));
 centralizeV($(".worktime"), true);
+centralizeV($(".warranty"), true);
+centralizeV($(".contacts"), true);
+
+tableZebra()
